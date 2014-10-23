64bd2a87075cb457db3603641f9de80c05884a48
Allan Simon <simona@gobeta.com.cn>
2014-10-23 23:31:23 +0800

    removed unecessary files

--- fourty_two/src/bin/test.rs
+++
@@ -1,9 +0,0 @@
-#![feature(phase)]
-#[phase(plugin, link)]
-//note: the name of the crate is unrelated with the macro's name
-extern crate fourty_two;
-
-fn main() {
-    println!("oh a int {}", fourty_two! {});
-
-}
